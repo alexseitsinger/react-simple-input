5c0e8c4340ad77e8ae576809607d98c7bc64d616
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-10-06 13:24:24 -0400

    chore: Uses new babel config.

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,16 @@
-{
-  "presets": [
+module.exports = {
+  presets: [
     ["@babel/preset-env", {
       "useBuiltIns": "entry",
       "corejs": 3
     }],
     "@babel/preset-react"
   ],
-  "plugins": [
+  plugins: [
     "@babel/plugin-proposal-export-namespace-from",
     "@babel/plugin-syntax-export-namespace-from",
     "@babel/plugin-transform-runtime",
     "babel-plugin-transform-react-remove-prop-types",
     "@babel/plugin-proposal-class-properties"
-  ]
+  ],
 }

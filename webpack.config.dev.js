f043d15e5d5e1c6e598ce1c1d202cb0087a73276
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-10-24 20:40:01 -0400

    style: Updates.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -16,8 +16,8 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: "babel-loader"
-      }
-    ]
+        use: "babel-loader",
+      },
+    ],
   },
 }
